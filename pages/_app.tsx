b48f38184eb731e8da896d369c76fa3e1432efae
Dylan McGuire <dylanmcguire@Dylans-MacBook-Pro-2.local>
2024-01-04 09:37:18 -0600

    setup skeleton for graph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app'
+import React from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Component {...pageProps} />
+    </DndProvider>
+  );
 }
+
+export default MyApp;
